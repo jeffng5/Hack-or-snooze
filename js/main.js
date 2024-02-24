0764628832a9479658bede238512e4c9d1d1e773
jeffng5 <jeffreykng@hotmail.com>
2024-02-24 06:05:31 -0500

    added all functionalities

--- js/main.js
+++ js/main.js
@@ -6,14 +6,22 @@ const $body = $("body");
 
 const $storiesLoadingMsg = $("#stories-loading-msg");
 const $allStoriesList = $("#all-stories-list");
+const $favoritedStories = $("#favorited-stories")
+const $ownStories = $("#my-stories")
+const $storiesContainer = $("#stories-container")
 
-const $loginForm = $("#login-form");
-const $signupForm = $("#signup-form");
+const $storiesLists = $(".stories-list")
+const $loginForm = $("#login-form")
+const $signupForm = $("#signup-form")
+
+const $submitForm = $("#submit-form")
+
+const $navSubmitStory = $("#nav-submit-story")
 
 const $navLogin = $("#nav-login");
-const $navUserProfile = $("#nav-user-profile");
+const $navUserProfile = $("#nav-user-profile")
 const $navLogOut = $("#nav-logout");
-
+const $userProfile = $("#user-profile");
 /** To make it easier for individual components to show just themselves, this
  * is a useful function that hides pretty much everything on the page. After
  * calling this, individual components can re-show just what they want.
@@ -22,8 +30,10 @@ const $navLogOut = $("#nav-logout");
 function hidePageComponents() {
   const components = [
     $allStoriesList,
+    $submitForm,
     $loginForm,
     $signupForm,
+    $userProfile
   ];
   components.forEach(c => c.hide());
 }
--- js/models.js
+++ js/models.js
@@ -73,11 +73,33 @@ class StoryList {
    * Returns the new Story instance
    */
 
-  async addStory( /* user, newStory */) {
-    // UNIMPLEMENTED: complete this function!
-  }
-}
+  async addStory( user, {title, author, url}) {
+    const token = user.loginToken;
+    const res = await axios({method : 'post', url: `${BASE_URL}/stories`, data : {token, story : {title, author, url}},
+  })
+  
+    const story = new Story(res.data.story);
+    this.stories.unshift(story);
+    user.ownStories.unshift(story)
 
+    return story
+}
+async removeStory(user, storyId) {
+  const token = user.loginToken;
+  await axios({
+    url: `${BASE_URL}/stories/${storyId}`,
+    method: "DELETE",
+    data: { token: user.loginToken }
+  });
+
+  // filter out the story whose ID we are removing
+  this.stories = this.stories.filter(story => story.storyId !== storyId);
+
+  // do the same thing for the user's list of stories & their favorites
+  user.ownStories = user.ownStories.filter(s => s.storyId !== storyId);
+  user.favorites = user.favorites.filter(s => s.storyId !== storyId);
+}
+}
 
 /******************************************************************************
  * User: a user in the system (only used to represent the current user)
@@ -193,4 +215,39 @@ class User {
       return null;
     }
   }
+  async addFavorite(story) {
+    this.favorites.push(story);
+    await this._addOrRemoveFavorite("add", story)
+  }
+
+  /** Remove a story to the list of user favorites and update the API
+   * - story: the Story instance to remove from favorites
+   */
+
+  async removeFavorite(story) {
+    this.favorites = this.favorites.filter(s => s.storyId !== story.storyId);
+    await this._addOrRemoveFavorite("remove", story);
+  }
+
+  /** Update API with favorite/not-favorite.
+   *   - newState: "add" or "remove"
+   *   - story: Story instance to make favorite / not favorite
+   * */
+
+  async _addOrRemoveFavorite(newState, story) {
+    const method = newState === "add" ? "POST" : "DELETE";
+    const token = this.loginToken;
+    await axios({
+      url: `${BASE_URL}/users/${this.username}/favorites/${story.storyId}`,
+      method: method,
+      data: { token },
+    });
+  }
+
+  /** Return true/false if given Story instance is a favorite of this user. */
+
+  isFavorite(story) {
+    return this.favorites.some(s => (s.storyId === story.storyId));
+  }
 }
+
